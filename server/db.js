1b4ef2258177388a52c19265c8d90e352bf3d388
tushar-glitch <tusharc20001@gmail.com>
2025-12-07 11:13:32 +0530

    DB issue resolved

--- server/db.js
+++ server/db.js
@@ -8,6 +8,10 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'nua_file_share',
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+        rejectUnauthorized: false
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
--- server/server.js
+++ server/server.js
@@ -13,18 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://nua-drive.vercel.app'],
     credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 
-// keyID:
-// 0037469dbc702530000000001
-// keyName:
-// nua-drive
-// applicationKey:
-// K003W+GTfxqo69yuuY/H+OMQFIyNyto
 
 // Routes
 app.use('/api/auth', authRoutes);
